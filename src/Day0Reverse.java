8ad864e5af65fb4f6d255612c47a87b109b944ed
HoangFalcon <hoangleftw@gmail.com>
2019-08-22 23:42:16 +0100

    Initial commit

---
+++ src/Day0Reverse.java
@@ -0,0 +1,16 @@
+public class Day0Reverse {
+
+    public static void main(String args[]) {
+        Day0Reverse test = new Day0Reverse();
+        test.doSomething(); // Pog
+
+    }
+
+    public Day0Reverse(){
+
+    }
+
+    public static void doSomething() {
+        System.out.println("XDddd");
+    }
+}
